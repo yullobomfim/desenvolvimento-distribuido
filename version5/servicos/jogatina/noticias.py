3f6bbc73df6a596c877d6bc44f604731a8fa282d
Yullo Costa Bomfim <35705562+yullobomfim@users.noreply.github.com>
2022-04-27 19:08:05 -0300

    feat: versao5

---
+++ version5/servicos/jogatina/noticias.py
@@ -0,0 +1,62 @@
+from flask import Flask, request
+from flask import jsonify
+from pymemcache.client import base
+
+servico = Flask(__name__)
+
+# "constantes"
+IS_ALIVE = "yes"
+VERSION = "0.0.1"
+AUTHOR = "Yullo Costa Bomfim"
+EMAIL = "yullo.bomfim@gmail.com"
+BANCO_VOLATIL = "banco_jogatina"
+PORTA_BANCO = 11211
+
+# rotas do meu servico
+# rota de ping (o cliente deve perguntar se o servico estah atendendo)
+
+
+@servico.route("/isalive/")
+def is_alive():
+    return IS_ALIVE
+
+# rota que retorna informacoes basicas sobre o servico e o autor do servico
+
+
+@servico.route("/info/")
+def get_info():
+    info = jsonify(
+        version=VERSION,
+        author=AUTHOR,
+        email=EMAIL
+    )
+
+    return info
+
+
+@servico.route("/gravar/", methods=["post", "get"])
+def gravar():
+    noticias = request.get_json()
+    if noticias:
+        cliente = base.Client((BANCO_VOLATIL, PORTA_BANCO))
+        cliente.set("jogatina", noticias)
+        cliente.close()
+
+    return "OK"
+
+
+# rota que retorna noticias sobre jogos eletronicos
+@servico.route("/noticias/")
+def get_jogatina():
+    cliente = base.Client((BANCO_VOLATIL, PORTA_BANCO))
+    noticias = cliente.get("jogatina")
+    cliente.close()
+
+    return noticias
+
+
+if __name__ == "__main__":
+    servico.run(
+        host="0.0.0.0",
+        debug=True
+    )
